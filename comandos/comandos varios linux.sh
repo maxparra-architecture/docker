32f34b571adef3cee26034286528f504823d5c35
Max Parra <mapaleu@gmail.com>
2021-06-05 07:39:47 -0500

    update

--- comandos/comandos varios linux.sh
+++ comandos/comandos varios linux.sh
@@ -1,3 +1,10 @@
 #Instalar netcat en ubuntu
 apt-get update
 apt-get install netcat
+
+#echo server con netcat
+nc -lp 45678 | nc -lp 45679
+
+#clientes terminales con netcat
+nc localhost 45678
+nc localhost 45679
